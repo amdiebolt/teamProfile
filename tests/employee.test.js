e2dcd31cff76d87c0714728ac4df78dac8e4e480
Alex Diebolt <alexander.diebolt@gmail.com>
2022-03-02 14:14:44 -0800

    finally done testing?

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,26 +1,26 @@
 const Employee = require("../lib/employee")
 
 test("getName()", () => {
-    const value = "rick";
-    const e = new Employee(value);
-    expect(e.getName()).toBe(value);
-  });
+    const value = "rick"
+    const e = new Employee(value)
+    expect(e.getName()).toBe(value)
+  })
   
   test("getId()", () => {
-    const value = 69;
-    const e = new Employee("Shh", value);
-    expect(e.getId()).toBe(value);
+    const value = 69
+    const e = new Employee("Shh", value)
+    expect(e.getId()).toBe(value)
   });
   
   test("getEmail()", () => {
     const value = "test@test.com";
-    const e = new Employee("fShh", 1, value);
-    expect(e.getEmail()).toBe(value);
-  });
+    const e = new Employee("fShh", 1, value)
+    expect(e.getEmail()).toBe(value)
+  })
   
   test("getRole()", () => {
-    const value = "Employee";
-    const e = new Employee("rick", 1, "test@test.com");
-    expect(e.getRole()).toBe(value);
-  });
+    const value = "Employee"
+    const e = new Employee("rick", 1, "test@test.com")
+    expect(e.getRole()).toBe(value)
+  })
   
